0624540578043a2a8a0aa45f32db2c34f677cf6c
En-Chieh Tseng <ents7354@colorado.edu>
2017-09-20 17:37:34 -0600

    CSCI1300 Assignment 3

---
+++ problem1.cpp
@@ -0,0 +1,24 @@
+// Author: Enchieh Tseng
+// Recitation: 110 – Monika Tak
+//
+// Assignment 3
+// Problem 1
+
+# include <iostream>
+using namespace std;
+
+/**
+* Algorithm description:
+*
+*
+*/
+
+void menu (chr a)
+{
+    cout << "Which story would you like to play? Enter the number of the story (1, 2, or 3) or type q to quit:";
+    cin >> a;
+    if (a != '1' || a != '2' || a !='3' || a !='q')
+    {
+
+    }
+}
