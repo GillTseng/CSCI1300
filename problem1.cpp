f7ea5dd3a447ccde8ba42f4e31cd137f4dd0be25
En-Chieh Tseng <ents7354@colorado.edu>
2017-09-21 20:52:05 -0600

    CSCI1300 Assignment3 problem1

--- problem1.cpp
+++ problem1.cpp
@@ -4,79 +4,81 @@
 // Assignment 3
 // Problem 1
 
+/**
+* Algorithm description:
+* The function is a story generator, the user will be asked to select a story (1, 2 or 3)
+* after the user select one of the options, the user will be asked to enter several strings
+* the strings that user inputted will be used in the story that they selected.
+*/
+
 # include <iostream>
 using namespace std;
 
-/**
-* Algorithm description:
-*
-*
-*/
+void menu ();
 
-void menu (char);
 int main()
 {
-    char a ='1';
-    menu(a);
+    menu();
 }
 
-void story1 (string plur_noun, string occupation, string animal, string place)
+void story1 (void)
 {
+    string plur_noun,occupation,animal,place;
+    cout << "Enter a plural noun: "<< endl;
+    cin >> plur_noun;
+    cout << "Enter an occupation: "<< endl;
+    cin >> occupation;
+    cout << "Enter an animal name: "<< endl;
+    cin >> animal;
+    cout << "Enter a place: "<< endl;
+    cin >> place;
     cout << "In the book War of the " << plur_noun << ", the main character is an anonymous " << occupation <<
     " who records the arrival of the " << animal << "s in " << place << "." << endl;
 }
 
-void story2 (string name, string state_country)
+void story2 (void)
 {
+    string name, state_country;
+    cout << "Enter a name: "<< endl;
+    cin >> name;
+    cout << "Enter a state/country: "<< endl;
+    cin >> state_country;
     cout << name << ", I've got a feeling we're not in " << state_country << " anymore." << endl;
 }
 
-void story3 (string first_name, string relative, string verb)
+void story3 (void)
 {
+    string first_name, relative, verb;
+    cout << "Enter a first name: "<< endl;
+    cin >> first_name;
+    cout << "Enter a relative: "<< endl;
+    cin >> relative;
+    cout << "Enter a verb: "<< endl;
+    cin >> verb;
     cout << "Hello. My name is " << first_name << ". You killed my " << relative << ". Prepare to " << verb <<"." << endl;
 }
 
 
-void menu (char a)
+void menu (void)
 {
+    char a;
     cout << "Which story would you like to play? Enter the number of the story (1, 2, or 3) or type q to quit:";
     cin >> a;
     if (a == '1')
     {
-        string plur_noun, occupation, animal, place;
-        cout << "Enter a plural noun: "<< endl;
-        cin >> plur_noun;
-        cout << "Enter an occupation: "<< endl;
-        cin >> occupation;
-        cout << "Enter an animal name: "<< endl;
-        cin >> animal;
-        cout << "Enter a place: "<< endl;
-        cin >> place;
-        story1(plur_noun,occupation,animal,place);
-        menu(a);
+        story1();
+        menu();
 
     }
     else if (a == '2')
     {
-        string name, state_country;
-        cout << "Enter a name: "<< endl;
-        cin >> name;
-        cout << "Enter a state/country: "<< endl;
-        cin >> state_country;
-        story2(name,state_country);
-        menu(a);
+        story2();
+        menu();
     }
     else if (a == '3')
     {
-        string first_name, relative, verb;
-        cout << "Enter a first name: "<< endl;
-        cin >> first_name;
-        cout << "Enter a relative: "<< endl;
-        cin >> relative;
-        cout << "Enter a verb: "<< endl;
-        cin >> verb;
-        story3(first_name,relative,verb);
-        menu(a);
+        story3();
+        menu();
     }
     else if (a == 'q')
     {
--- problem2.cpp
+++ problem2.cpp
@@ -8,16 +8,38 @@
 # include <cmath>
 using namespace std;
 
+/**
+* Algorithm description:
+* Calculate wind chill when given air temperature in Fahrenheit(T) and wind speed(V).
+* Wind chill = 35.74 + 0.6215 * T - 35.75 * V^0.16 + 0.4275 * T * V^0.16)
+* Return wind chill.
+*/
 
-int windChillCalculator (double T, double V)
+float windChillCalculator (float T, float V)
 {
-    double win_chill;
+    float win_chill;
     win_chill = 35.74 + 0.6215 * T - 35.75 * pow(V,0.16) + 0.4275 * T * pow(V,0.16);
     return win_chill;
 }
 
+
+/**
+* Algorithm description:
+* When given air temperature in Fahrenheit and range of wind speed, print wind chill that increment by wind speed step.
+*/
+void printWindChill (float T, float low_wind_speed, float high_wind_speed, float wind_speed_step)
+{
+    float V = low_wind_speed;
+    while (V >= low_wind_speed && V <= high_wind_speed)
+    {
+        cout << "The wind chill is " << windChillCalculator(T,V) << " degrees F for an air temperature of " << T <<
+        " degrees F" << " and a wind speed of " << V << " mph." << endl;
+        V = V + wind_speed_step;
+
+    }
+}
+
 int main()
 {
-    cout.precision(4);
-    cout << windChillCalculator(30.0,5.0);
+    printWindChill(30.0,5.0,8.0,1.0);
 }
