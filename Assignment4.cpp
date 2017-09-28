52a4f10a77199ff8a1094c7d61fe1bfaf4da63e1
En-Chieh Tseng <ents7354@colorado.edu>
2017-09-28 16:45:03 -0600

    CSCI 1300 Assignment 4

--- Assignment4.cpp
+++ Assignment4.cpp
@@ -9,20 +9,95 @@ using namespace std;
 
 float similarityScore (string sequence1, string sequence2)
 {
-    if (sequence1.length() == sequence2.length())
+    if ((int)sequence1.length() == (int)sequence2.length())
     {
         int i =0;
         int num_match =0;
-        while (i < sequence1.length())
+        while (i < (int)sequence1.length())
         {
             if (sequence1[i] == sequence2[i])
             {
-                num_match = num_match+1;
+                num_match++;
             }
+            i=i+1;
         }
+        return num_match / (int)sequence1.length();
     }
     else
     {
         return 0;
     }
+}
+
+
+int countMatches (string genome, string sequence1 , float min_score)
+{
+
+    int i = 0;
+    int len = sequence1.length();
+    int count = 0;
+    while ((i+len) <= (int)genome.length())
+    {
+        int j =0;
+        int sub_count =0;
+        while ( j < len)
+        {
+            if ( genome[j] == sequence1[j] )
+            {
+                sub_count++;
+            }
+            j++;
+        }
+        float simi_score;
+        simi_score = sub_count/len;
+        if (simi_score > min_score)
+        {
+            count++;
+        }
+        i++;
+    }
+    return count;
+}
+
+
+float findBestMatch (string genome, string seq)
+{
+    float highest_score =0;
+    float score;
+    int i = 0;
+    int len = seq.length();
+    while ( (i+len) < (int)genome.length())
+    {
+        score = similarityScore(genome.substr(i,len),seq);
+        if ( score > highest_score)
+        {
+            highest_score = score;
+        }
+        i++;
+    }
+    return highest_score;
+}
+
+int findBestGenome (string genome1, string genome2, string genome3, string seq)
+{
+    float highest_1 = findBestMatch(genome1,seq);
+    float highest_2 = findBestMatch(genome2,seq);
+    float highest_3 = findBestMatch(genome3,seq);
+    if ( highest_1 > highest_2 && highest_1 > highest_3 )
+    {
+        return 1;
+    }
+    if ( highest_2 > highest_1 && highest_2 > highest_3 )
+    {
+        return 2;
+    }
+    if ( highest_3 > highest_1 && highest_3 > highest_2 )
+    {
+        return 3;
+    }
+    else
+    {
+        return 0;
+    }
+
 }
