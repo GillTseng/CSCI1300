adcf23e80901d5d8115cb71fe053f5c0f4d1b1c6
En-Chieh Tseng <ents7354@colorado.edu>
2017-11-01 13:20:18 -0600

    CSCI1300 Recitation 10

---
+++ Recitation10/planet.cpp
@@ -0,0 +1,33 @@
+#include "planet.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+planet::planet(){
+}
+
+planet::planet(string planetName, float planetRadius){
+	name = planetName;
+	radius = planetRadius;
+}
+
+planet::~planet(){
+	//letting compiler handle deconstruction of the class
+}
+
+void planet::setName(string planetName){
+	name = planetName;
+}
+
+string planet::getName(){
+	return name;
+}
+
+void planet::setRadius(float planetRadius){
+	radius = planetRadius;
+}
+
+float planet::getRadius(){
+	return radius;
+}
+
---
+++ Recitation10/solarSystem.cpp
@@ -0,0 +1,59 @@
+#include "solarSystem.h"
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+solarSystem::solarSystem(){
+}
+
+solarSystem::solarSystem(string name){
+    systemName = name;
+    numPlanets = 0;
+}
+
+solarSystem::~solarSystem(){
+}
+
+//string solarSystem::findLargest(){  // return the name of the largest planet
+//    int max_index = 0;
+//    for (int i = 0; i < numPlanets; i++){
+//        if ( planetDiameters[i] > planetDiameters[max_index]){
+//            max_index = i;
+//        }
+//    }
+//    return planetNames[max_index];
+//}
+
+string solarSystem::getName(){      // return the name of the solar system
+    return systemName;
+}
+
+//float solarSystem::getDiameter(){   // return the diameter of the largest orbit
+//    int max_index = 0;
+//    for (int i = 0 ; i < numPlanets; i++){
+//        if ( planetOrbitDist[i] > planetOrbitDist[max_index]){
+//            max_index = i;
+//        }
+//    }
+//    return planetOrbitDist[max_index]*2;
+}
+
+int solarSystem::getNumPlanets(){         // return the number of planets in the system
+    return numPlanets;
+}
+
+bool solarSystem::addPlanet(string name, float ){           //Adds a single planet to solar system
+    if(numPlanets < maxNumPlanets){
+        planetNames[numPlanets] = name;
+		planetDiameters[numPlanets] = 2*planetRadius;
+		planetOrbitDist[numPlanets] = planetDist;
+		numPlanets++;
+		return numPlanets != 0;
+    }
+    return numPlanets==0;
+}
+
+planet solarSystem::getPlanet(int index){           // return an object of planet at provided index
+    systemPlanets[index];
+}
